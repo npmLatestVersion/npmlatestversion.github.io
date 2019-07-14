1946d56a8c294b93560a066723bff28194c16e20
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-07-14 16:37:25 +0530

    add console.log

--- src/PackageDetail.js
+++ src/PackageDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Search from './Search'
+import { Link } from "react-router-dom";
 
 function PackageDetail({match}) {
 
@@ -12,18 +13,21 @@ function PackageDetail({match}) {
             })    
             .then(function(res) {
                 document.title = `${match.params.packageName} package latest version on npm`;
-                setVersion(JSON.stringify(res))
+                console.log(res)
+                setVersion(res)
             })
     });
   
     return (
-        <div>
+    <div>
       <Search />
-      <div className="p-3 mt-4 border-l-4">
+      <Link to='/{match.params.packageName}'>
+      <div className="p-3 mt-4">
           <p className="font-bold text-xl mb-2"><a href="https://www.npmjs.com/package/{match.params.packageName}">{match.params.packageName}</a></p>
           <p className="text-gray-700 text-base">Version: <span id="latest">{version}</span></p>
           <p className="mt-3"><code className="bg-gray-200 px-3 py-1">npm install {match.params.packageName}@latest</code></p>
       </div>
+      </Link>
       </div>
  
     );
