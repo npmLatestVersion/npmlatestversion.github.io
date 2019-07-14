0b84a4d384dd6c39eeba7d4f2a77db9d4c1219bf
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-07-14 16:14:16 +0530

    create search

--- src/PackageDetail.js
+++ src/PackageDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import Search from './Search'
 
 function PackageDetail({match}) {
 
@@ -17,11 +17,15 @@ function PackageDetail({match}) {
     });
   
     return (
+        <div>
+      <Search />
       <div className="p-3 mt-4 border-l-4">
           <p className="font-bold text-xl mb-2"><a href="https://www.npmjs.com/package/{match.params.packageName}">{match.params.packageName}</a></p>
           <p className="text-gray-700 text-base">Version: <span id="latest">{version}</span></p>
           <p className="mt-3"><code className="bg-gray-200 px-3 py-1">npm install {match.params.packageName}@latest</code></p>
       </div>
+      </div>
+ 
     );
 }
 
